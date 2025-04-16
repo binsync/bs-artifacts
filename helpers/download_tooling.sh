50154fcfe5feb7510e38f9eab6f4ac32717de43b
mahaloz <zion@zionbasque.com>
2025-04-15 23:43:53 -0700

    silence

--- helpers/download_tooling.sh
+++ helpers/download_tooling.sh
@@ -1,3 +1,3 @@
-wget "https://github.com/binsync/bs-artifacts/releases/download/v0.0.0/tooling-9.0.tar.gz.enc"
+wget -q "https://github.com/binsync/bs-artifacts/releases/download/v0.0.0/tooling-9.0.tar.gz.enc"
 openssl enc -d -aes-256-cbc -pbkdf2 -in tooling-9.0.tar.gz.enc -out tooling-9.0.tar.gz -pass pass:$TOOLING_KEY
 rm -r tooling-9.0.tar.gz.enc
