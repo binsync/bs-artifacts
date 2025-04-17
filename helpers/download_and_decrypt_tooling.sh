f23656cce74cb3d1bc21dbb3796b773178fe79f6
mahaloz <zion@zionbasque.com>
2025-04-17 08:54:37 -0700

    fixup files

--- helpers/download_and_decrypt_tooling.sh
+++ helpers/download_and_decrypt_tooling.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
 wget -q "https://github.com/binsync/bs-artifacts/releases/download/v0.0.0/tooling-9.0.tar.gz.enc"
 openssl enc -d -aes-256-cbc -pbkdf2 -in tooling-9.0.tar.gz.enc -out tooling-9.0.tar.gz -pass pass:$TOOLING_KEY
-rm -r tooling-9.0.tar.gz.enc
+openssl enc -d -aes-256-cbc -pbkdf2 -in runner.reg.enc -out runner.reg -pass pass:$TOOLING_KEY
+rm -r tooling-9.0.tar.gz.enc runner.reg.enc
---
+++ helpers/setup_ida_ci.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# downloads and decrypts files
+./helpers/download_tooling.sh
+# decompress them
+tar -xf tooling-9.0.tar.gz
+# setup for normalized use (see refile)
+mv tooling-9.0 ~/idapro-9.0 && \
+    mkdir -p ~/.idapro && \
+    cp runner.reg ~/.idapro/ida.reg
+# install ida python
+python -m pip install ~/idapro-9.0/idalib/python \
+    && python ~/idapro-9.0/idalib/python/py-activate-idalib.py \
+    && echo -e "0\n" | ./idapro-9.0/idapyswitch
